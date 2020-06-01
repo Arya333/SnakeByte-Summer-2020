6aab5f267d6713f9e236ba1262870dd12f75b627
Arya Anantula <43790515+Arya333@users.noreply.github.com>
2020-05-31 20:44:19 -0500

    movement methods

--- TeamCode/src/main/java/Library/Drivetrain.java
+++ TeamCode/src/main/java/Library/Drivetrain.java
@@ -2,13 +2,16 @@ package Library;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class Drivetrain {
+public class Drivetrain{
     public DcMotor motorFL;
     public DcMotor motorFR;
     public DcMotor motorBL;
     public DcMotor motorBR;
     LinearOpMode opMode;
+    public double countsPerInch;
+    ElapsedTime time;
 
     public Drivetrain(LinearOpMode opMode) throws InterruptedException{
         this.opMode = opMode;
@@ -16,6 +19,10 @@ public class Drivetrain {
         motorFR = this.opMode.hardwareMap.dcMotor.get("motorFR");
         motorBL = this.opMode.hardwareMap.dcMotor.get("motorBL");
         motorBR = this.opMode.hardwareMap.dcMotor.get("motorBR");
+
+        countsPerInch = EncodersPerInch(560, 0.5, (100/25.4));
+
+        time = new ElapsedTime();
     }
 
     public void startMotors(double left, double right){
@@ -23,6 +30,31 @@ public class Drivetrain {
         motorFR.setPower(right);
         motorBL.setPower(left);
         motorBR.setPower(right);
+    }
+
+    public double EncodersPerInch(double encoders, double gearReduction, double wheelDiameter){
+        return ((encoders * gearReduction) /(wheelDiameter * Math.PI) );
+    }
+
+    public double getEncoderAvg(){
+        int count = 4;
+        if (motorFL.getCurrentPosition() == 0){
+            count--;
+        }
+        if (motorFR.getCurrentPosition() == 0){
+            count--;
+        }
+        if (motorBL.getCurrentPosition() == 0){
+            count--;
+        }
+        if (motorBR.getCurrentPosition() == 0){
+            count--;
+        }
+        if (count == 0) count++;
+        return (Math.abs(motorFL.getCurrentPosition()) +
+                Math.abs(motorFR.getCurrentPosition()) +
+                Math.abs(motorBL.getCurrentPosition()) +
+                Math.abs(motorBR.getCurrentPosition())) / count;
     }
 
     public void resetEncoders(){
@@ -43,5 +75,19 @@ public class Drivetrain {
         opMode.idle();
         motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         opMode.idle();
+    }
+
+    public void moveInches(double inches, double power){
+        resetEncoders();
+        while (getEncoderAvg() < inches * countsPerInch){
+            startMotors(power, power);
+        }
+    }
+
+    public void moveTime(int millis, double power){
+        time.reset();
+        while (time.milliseconds() < millis){
+            startMotors(power, power);
+        }
     }
 }
---
+++ TeamCode/src/main/java/OpModes/Auto1.java
@@ -0,0 +1,22 @@
+package OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import Library.Drivetrain;
+
+@Autonomous(name = "SampleAuto", group = "4546")
+public class Auto1 extends LinearOpMode {
+    private Drivetrain drivetrain;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drivetrain = new Drivetrain(this);
+
+        waitForStart();
+
+        drivetrain.moveInches(10, .5);
+        sleep(1000);
+        drivetrain.moveTime(3000, .6);
+    }
+}
