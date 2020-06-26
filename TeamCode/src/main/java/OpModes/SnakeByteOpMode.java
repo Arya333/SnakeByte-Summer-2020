f95ea00d8518baed334faedea91dfc16cce52f94
Arya Anantula <43790515+Arya333@users.noreply.github.com>
2020-06-26 15:12:29 -0500

    Gyro & TeleOp added

---
+++ TeamCode/src/main/java/OpModes/SnakeByteOpMode.java
@@ -0,0 +1,49 @@
+package OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public abstract class SnakeByteOpMode extends OpMode {
+
+    DcMotor motorFL;
+    DcMotor motorFR;
+    DcMotor motorBL;
+    DcMotor motorBR;
+
+    public void init(){
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+
+        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void startMotors(double l, double r){
+        motorFL.setPower(l);
+        motorBL.setPower(l);
+        motorFR.setPower(r);
+        motorBR.setPower(r);
+    }
+
+    public void stopMotors(){
+        motorFL.setPower(0);
+        motorBL.setPower(0);
+        motorFR.setPower(0);
+        motorBR.setPower(0);
+    }
+
+
+}
---
+++ TeamCode/src/main/java/OpModes/SnakeByteTeleOp.java
@@ -0,0 +1,49 @@
+package OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "TeleOp", group = "4546")
+public class SnakeByteTeleOp extends SnakeByteOpMode {
+
+    boolean tank = false;
+
+    public void loop() {
+        double left = 0;
+        double right = 0;
+        double max;
+
+        //speed constant (half speed when left bumper pressed)
+        double sC;
+        if (gamepad1.left_bumper) {
+            sC = .5;
+        }
+        else {
+            sC = 1;
+        }
+
+        //tank drive controls (Y value of left stick is for left side of drivetrain & vice versa)
+        if (tank) {
+            if (Math.abs(gamepad1.left_stick_y) > .1 || (Math.abs(gamepad1.right_stick_y)) > .1) {
+                startMotors(gamepad1.left_stick_y * sC, gamepad1.right_stick_y * sC);
+            } else {
+                stopMotors();
+            }
+        } else { //arcade drive controls
+            if (Math.abs(gamepad1.left_stick_y) > .1 || (Math.abs(gamepad1.right_stick_x)) > .1) {
+                left = (gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) -
+                        (gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x));
+                right = (gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y)) +
+                        (gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x));
+                max = Math.max(Math.abs(left), Math.abs(right));
+                if (max > 1.0) {
+                    left /= max;
+                    right /= max;
+                }
+
+                startMotors(left * sC, right * sC);
+            } else {
+                stopMotors();
+            }
+        }
+    }
+}
